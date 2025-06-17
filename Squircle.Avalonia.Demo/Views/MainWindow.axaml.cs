69130d2ff1f4d2d2b29d6a41ce78d6b6c1026ca7
Alexandr Pidanov <alexandrpi345@gmail.com>
2025-06-17 23:22:26 +0300

    Create demo app.

--- Squircle.Avalonia.Demo/Views/MainWindow.axaml.cs
+++ Squircle.Avalonia.Demo/Views/MainWindow.axaml.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Diagnostics;
+using Avalonia;
 using Avalonia.Controls;
 
 namespace Squircle.Avalonia.Demo.Views;
@@ -7,5 +10,20 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        SquircleCornerRadiusTextBox.TextChanged += (_, _) =>
+        {
+            try
+            {
+                Squircle.CornerRadius = CornerRadius.Parse(SquircleCornerRadiusTextBox.Text ?? string.Empty);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+        };
+        
+        SquircleCornerSmoothingSlider.ValueChanged += (_, _) =>
+            Squircle.CornerSmoothing = SquircleCornerSmoothingSlider.Value;
     }
 }
